0dad2d7c9d17adf0783f1a8e671c54a1318d88c8
Damdrara <github@damdrara.com>
2023-12-03 00:43:35 +0100

    Added Work Order Resources

--- colony_workorders.lua
+++ colony_workorders.lua
@@ -18,17 +18,7 @@ basalt.schedule(function()
     while true do
 
         local workOrders = colony.getWorkOrders()
-        local labelsNeeded = 1 + (#workOrders * 1)
-
-        while #labels < labelsNeeded do
-            local line = #labels + 2
-            table.insert(labels, main:addLabel():setText(""):setPosition(2, line))
-        end
-
-        while #labels > labelsNeeded do
-            labels[#labels]:remove()
-            table.remove(labels)
-        end
+        local labelsNeeded = 1
 
         if #workOrders > 0 then
             labels[1]:setText("Work Orders: " .. #workOrders)
@@ -37,12 +27,38 @@ basalt.schedule(function()
         end
 
         local line = 2
-        for k, v in ipairs(workOrders) do
-            labels[line]
-                :setText(v.workOrderType .. ", " .. v.buildingName .. " lvl" .. v.targetLevel)
+        for k, workOrder in ipairs(workOrders) do
 
-            line = line + 1
+            local resources = colony.getWorkOrderResources(workOrder.id)
+            local workOrderLines = 1 + #resources
+
+            labelsNeeded = labelsNeeded + workOrderLines
+            while #labels < labelsNeeded do
+                table.insert(labels, main:addLabel():setText(""):setPosition(2, #labels + 2))
+            end
+
+            labels[line]:setText(workOrder.workOrderType .. " " .. workOrder.buildingName .. " lvl" .. workOrder.targetLevel)
+
+            local idx = 1;
+            for i, resource in ipairs(resources) do
+                if resource.available > resource.needed then
+                    workOrderLines = workOrderLines - 1
+                    labelsNeeded = labelsNeeded - 1
+                else
+                    local needed = resource.needed - resource.available
+                    labels[line + idx]:setText(" " .. string.format("%4d", needed) .. "x " .. resource.displayName)
+                    idx = idx + 1
+                end
+            end
+
+            line = line + workOrderLines
         end
+
+        while #labels > labelsNeeded do
+            labels[#labels]:remove()
+            table.remove(labels)
+        end
+
         sleep(1)
     end
 end)()
