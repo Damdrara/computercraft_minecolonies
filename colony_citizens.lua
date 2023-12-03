99cab5c085d19823c54fd158cfa99237b3fb2e64
Damdrara <github@damdrara.com>
2023-12-03 02:33:08 +0100

    Improved citizen list for PDA

--- colony_citizens.lua
+++ colony_citizens.lua
@@ -41,28 +41,20 @@ local main = basalt.createFrame()
 local labels = {
 }
 
-function TableConcat(t1,t2)
-    for i=1,#t2 do
-        t1[#t1+1] = t2[i]
-    end
-    return t1
-end
-
 basalt.schedule(function()
     while true do
 
         local citizens = colony.getCitizens()
-        --citizens = TableConcat(citizens, citizens)
-        --citizens = TableConcat(citizens, citizens)
 
-        local labelsNeeded = #citizens * 4
+        local labelsNeeded = #citizens * 5
 
         while #labels < labelsNeeded do
-            local line = 2 + (#labels * 3/4)
+            local line = #labels + 2
             table.insert(labels, main:addLabel():setText(""):setPosition(2, line))
-            table.insert(labels, main:addLabel():setText(""):setPosition("parent.w - 1", line))
-            table.insert(labels, main:addLabel():setText(""):setPosition(2, line + 1))
             table.insert(labels, main:addLabel():setText(""):setPosition(3, line + 1))
+            table.insert(labels, main:addLabel():setText(""):setPosition(3, line + 2))
+            table.insert(labels, main:addLabel():setText(""):setPosition(3, line + 2))
+            table.insert(labels, main:addLabel():setText(""):setPosition(3, line + 3))
         end
 
         while #labels > labelsNeeded do
@@ -83,8 +75,18 @@ basalt.schedule(function()
             end
 
             labels[line]
-                :setText(v.name .. " (" .. gender .. ", " .. job .. ")")
+                :setText(v.name)
                 :setForeground(colors.black)
+
+            labels[line + 1]
+                :setText(gender .. ", " .. job)
+                :setForeground(colors.black)
+
+            local happyString = "Happyness: " .. tostring(math.floor(v.happiness * 10)/10) .. " | "
+
+            labels[line+2]
+                :setText(happyString)
+                :setForeground(colors.gray)
 
             local healthColor = colors.gray
             if v.health < v.maxHealth  then
@@ -93,29 +95,21 @@ basalt.schedule(function()
 
             local healthText = v.health .. "/" .. v.maxHealth
 
-            labels[line+1]
+            labels[line+3]
                 :setText(healthText)
-                :setPosition("parent.w - " .. string.len(healthText))
+                :setPosition(string.len(happyString) + 3)
                 :setForeground(healthColor)
-
-
-            local happyString = " Happyness: " .. tostring(math.floor(v.happiness * 10)/10) .. " | "
-
-            labels[line+2]
-                :setText(happyString)
-                :setForeground(colors.gray)
 
             local stateColor = colors.gray
             if (v.state == "Sick") then
                 stateColor = colors.red
             end
 
-            labels[line+3]
+            labels[line+4]
                 :setText(v.state)
-                :setPosition(string.len(happyString) + 2)
                 :setForeground(stateColor)
 
-            line = line + 4
+            line = line + 5
         end
 
         sleep(1)
